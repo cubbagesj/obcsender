0187b65f6bba0a73cc0863a677bb57e28384939c
cubbagesj <samuel.cubbage@navy.mil>
2018-01-02 14:08:00 -0500

    Tweaks to get it working

--- obcsender.py
+++ obcsender.py
@@ -2,20 +2,30 @@
 # This program is part of the Autonomous Model Software Package
 #
 # obcsender.py - A program to read an obc file and recreate CB12 OBC data pkts
+# This program needs to be modified whenever the CB12 packet map changes
+#
+# To use this program, the CB12 version of AM_Control needs to be modified
+# to look for ADCP velocity in the Z_lin_accel channel and to manually
+# set the script speed and depth
 #
 
 import socket, struct
 import time
 
 # Socket setup - send straight to Control computer
+# which is running on frmpi1
+#
 addr = ("192.168.1.210", 3188)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(0.5)
 
 # Define the OBC message formats
-obcAfmt = '>HHL679xHdHdHd93x'
-obcBfmt = '>458xHHHHhhhhhhhhhhhh320x'
+# This format has to change whenever the packet map changes
+#
+# CB12 splits the OBC data into two UDP packets
+obcAfmt = '>HHL551xHdHdHd157x'
+obcBfmt = '>394xHHHHhhhhhhhhhhhh320x'
 
 # Read in the OBC file and then loop to send packets
 
@@ -31,6 +41,8 @@ for record in obcfile:
 
 
     # Pack the data into the message
+    # 
+    # Message1 contains the depth gauges 
     message1 = struct.pack(obcAfmt, 
             0xf0c1,
 #            int(data[1]),
@@ -39,11 +51,11 @@ for record in obcfile:
             1,
             (float(data[11])*0.00178771),
             1,
-            #(float(data[12])*0.00176125),
             (float(data[12])*23.15228962),
             1,
             (float(data[13])*0.00176125))
 
+    # LN200 and ADCP come in message 2
     # Putting ADCP xvel in z-lin-accel for now
     message2 = struct.pack(obcBfmt,
             1,
