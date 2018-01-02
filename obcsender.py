c6eaec32ff600f8e64f0b35faf6fdfbeaba0af9c
cubbagesj <samuel.cubbage@navy.mil>
2018-01-02 12:21:54 -0500

    Initial commit - obcsender for CB12 SSN23

---
+++ obcsender.py
@@ -0,0 +1,76 @@
+#
+# This program is part of the Autonomous Model Software Package
+#
+# obcsender.py - A program to read an obc file and recreate CB12 OBC data pkts
+#
+
+import socket, struct
+import time
+
+# Socket setup - send straight to Control computer
+addr = ("192.168.1.210", 3188)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(0.5)
+
+# Define the OBC message formats
+obcAfmt = '>HHL679xHdHdHd93x'
+obcBfmt = '>458xHHHHhhhhhhhhhhhh320x'
+
+# Read in the OBC file and then loop to send packets
+
+obcfilename = 'run-1321.obc'
+
+pktcnt = 0
+
+obcfile = open(obcfilename, 'r')
+for record in obcfile:
+    record = record.rstrip()
+    data = record.split()
+
+
+
+    # Pack the data into the message
+    message1 = struct.pack(obcAfmt, 
+            0xf0c1,
+#            int(data[1]),
+            pktcnt,
+            0,
+            1,
+            (float(data[11])*0.00178771),
+            1,
+            #(float(data[12])*0.00176125),
+            (float(data[12])*23.15228962),
+            1,
+            (float(data[13])*0.00176125))
+
+    # Putting ADCP xvel in z-lin-accel for now
+    message2 = struct.pack(obcBfmt,
+            1,
+            0,
+            0,
+            0,
+            int(float(data[246])),
+            int(float(data[196])),
+            int(float(data[197])),
+            int(float(data[198])),
+            int(float(data[199])),
+            int(float(data[200])),
+            int(float(data[201])),
+            int(float(data[202])),
+            int(float(data[203])),
+            int(float(data[204])),
+            int(float(data[205])),
+            int(float(data[206])))
+
+
+    print(float(data[220])*(.045))
+    # Send the two OBC pkts
+    s.sendto(message1, addr)
+    s.sendto(message2, addr)
+
+    pktcnt += 1
+    # Wait to send next packet
+    time.sleep(.01)
+
+
